7f975bff419ff0e617ef87422650612ab3980547
jaxon grosam <jaxon.grosam@gmail.com>
2023-10-19 16:14:20 -0500

    finish setting up thought model with reaction subdoc and index for models

---
+++ models/Thought.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const dateFormat = (timestamp) => {
+  return new Date(timestamp).toLocaleString();
+};
+
+// Reaction subdocument
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+});
+
+// Thought schema referencing reaction subdoc
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
